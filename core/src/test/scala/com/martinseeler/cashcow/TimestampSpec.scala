6c187b5c08af891156675d263af6cc0cad84fbb7
Martin Seeler <mseeler@avantgarde-labs.de>
2016-08-14 22:07:57 +0200

    Add TimestampSpec

---
+++ core/src/test/scala/com/martinseeler/cashcow/TimestampSpec.scala
@@ -0,0 +1,41 @@
+package com.martinseeler.cashcow
+
+import com.martinseeler.cashcow.Timestamp._
+import org.scalacheck.Prop._
+import org.scalacheck._
+import org.scalatest.Matchers
+import shapeless.tag.@@
+
+object TimestampSpec extends Properties("Timestamp") with Matchers {
+
+  property("fromInt without a precision tag") = forAll { x: Int =>
+    "val p: Timestamp = x" shouldNot compile
+    true
+  }
+
+  property("fromInt with millisecond precision") = forAll { x: Int =>
+    val p: Timestamp @@ Millis = x
+    p.value == x
+  }
+
+  property("fromInt with nanosecond precision") = forAll { x: Int =>
+    val p: Timestamp @@ Nanos = x
+    p.value == x
+  }
+
+  property("fromLong without a precision tag") = forAll { x: Long =>
+    "val p: Timestamp = x" shouldNot compile
+    true
+  }
+
+  property("fromLong with millisecond precision") = forAll { x: Long =>
+    val p: Timestamp @@ Millis = x
+    p.value == x
+  }
+
+  property("fromLong with nanosecond precision") = forAll { x: Long =>
+    val p: Timestamp @@ Nanos = x
+    p.value == x
+  }
+
+}
