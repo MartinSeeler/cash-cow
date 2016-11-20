5b8f3f4bc2000286bde265d6fe5f7b888f5f4e98
Martin Seeler <developer@chasmo.de>
2016-11-20 11:06:15 +0100

    Add mid price to quote

--- core/src/main/scala/cashcow/Quote.scala
+++ core/src/main/scala/cashcow/Quote.scala
@@ -4,4 +4,8 @@ import cashcow.Price.{Ask, Bid}
 import cashcow.Timestamp.Nanos
 import shapeless.tag.@@
 
-case class Quote(time: Timestamp @@ Nanos, bid: Price @@ Bid, ask: Price @@ Ask)
+case class Quote(time: Timestamp @@ Nanos, bid: Price @@ Bid, ask: Price @@ Ask) {
+
+  val mid: Price = (bid.value + ask.value) / 2.0
+
+}
